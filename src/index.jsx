a42d403ecc97318d3a2d239969996280ba66b029
Srikshith Rao <srikshith5120@gmail.com>
2026-02-27 23:16:02 +0530

    Fix ThemeProvider wrapping in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
+import { ThemeProvider } from './contexts/ThemeContext.jsx';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <ThemeProvider>
+        <App />
+    </ThemeProvider>
+);
